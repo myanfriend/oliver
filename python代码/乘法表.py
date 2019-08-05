cf0fa65517f2fc8988314e44048b7b6d8d3b4dd4
myanfriend <myanfriend@163.com>
2019-08-05 21:58:58 +0800

    Update 乘法表.py

--- python代码/乘法表.py
+++ python代码/乘法表.py
@@ -1,15 +1,26 @@
+# def mutil_table():
+#     """
+#    9*9乘法表
+#     """
+#     i=1
+#
+#     sum=0
+#     while i<=9:
+#         j = 1
+#         while j<=i:
+#             sum=i*j
+#             print("%d*%d=%d"%(j,i,sum),end=(" "))
+#             j+=1
+#         print("")
+#         i+=1
+
 def mutil_table():
+    for i in range(1,10):
+        for j in range(1,i+1):
+            print("%d*%d=%2d"%(j,i,i*j),end="\t")
 
-    i=1
-
-    sum=0
-    while i<=9:
-        j = 1
-        while j<=i:
-            sum=i*j
-            print("%d*%d=%d"%(j,i,sum),end=(" "))
-            j+=1
         print("")
-        i+=1
 
+
+# mutil_table()
 mutil_table()
